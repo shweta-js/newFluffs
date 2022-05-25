705e4098418b070b873a7f63565bbef1b975d72d
Shweta.js <shwetajs200@gmail.com>
2022-05-25 14:54:50 +0530

    Update Settings.js

--- src/container/Settings.js
+++ src/container/Settings.js
@@ -1,14 +1,17 @@
 import React from 'react'
-import Heading from "../components/Heading"
+import "./Settings.css"
 const Settings = () => {
+    const [summary, setSummary] = React.useState('')
+    function handleSubmit(e) {
+        e.preventDefault();    console.log('You clicked submit.');
+      }
   return (
-    <div>
-        <Heading data="Settings"/>
-        edit profile
-        privacy
-        Security
-        <button  className="up-sty-btn"
-         >Signout</button>
+    <div className="settings">
+        <h4 className="setting-heading">See you soon</h4>
+        <form onSubmit={handleSubmit}>
+      <button  className="logout-submit">Logout</button>
+    </form>
+        
     </div>
   )
 }
